df6748e9ed8f09794de89608815f233d8f304cbe
SerhiiHaker <sergeytrader080808@gmail.com>
2022-04-30 22:31:03 +0300

    new js file

---
+++ menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	$('.nav a [href^="#"]').click(function () {/* Вибрати силки із меню */
+		var offset = $('.nav').innerHeight();/* висота меню, отримуємо динамічно */
+		var target = $(this).attr('href');/* зберігаємо значення атрибута href */
+		$('html, body').animate({
+			scrollTop: $(target).offset().top - offset /* віднімаємо висоту меню */
+		})
+	}, 500);/* час анімації */
+	$('.nav a [href^="#"]').removeClass('active');/* видаляємо клас active у всіх пунктах меню */
+	$(this).addClass('active');/* добавляємо клас active активній силці */
+	return false;
+})
